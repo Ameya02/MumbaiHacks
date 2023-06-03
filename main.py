ada19eb33687a35d09b7f89589ef849917817a8c
Ameya02 <65841021+Ameya02@users.noreply.github.com>
2023-06-03 23:43:12 +0530

    new files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template,url_for, request,redirect, jsonify
-import cohere
+import openai
+import os
 app = Flask(__name__,template_folder='template')
 
 
@@ -12,6 +13,7 @@ def index():
 def process():
     data = request.json
     # # Process the data as needed
+    resp1,resp2=easyexplain(data["input"])
     # res=data
     # print(data["input"])
     # return jsonify(res)
@@ -20,17 +22,19 @@ def process():
     # Indian economy: Right from independence, the modernisation of the economy, economic self-sufficiency, and social justice have been the characteristics of the Indian economy. India wanted to acquire modernity and self-reliance by establishing industries. We wanted to establish an economy based on social justice through planning. For this, the National Planning Commission was established to coordinate development through the policy of Five Year Plans.
     # The Narasimha Rao Government started economic reforms from 1991. These economic reforms are called economic liberalisation. The Indian economy flourished as a result of the implementation of this policy. Foreign investment in India increased. Skilled Indian professionals helped reform the Indian economy. The field of information technology opened several avenues of employment in the country. The changes after 1991 are also described as 'globalisation'.
     # '''
+    # co = cohere.Client(os.getenv("COHERE_API_KEy")}  # This is your trial API key
 
-    response = co.generate(
-        model='command-light-nightly',
-        prompt= data["input"],
-        max_tokens=1400,
-        temperature=1.1,
-        k=0,
-        stop_sequences=[],
-        return_likelihoods='NONE'
-    )
-    data = {"text":response.generations[0].text}
+
+    # response = co.generate(
+    #     model='command-light-nightly',
+    #     prompt= data["input"],
+    #     max_tokens=1400,
+    #     temperature=1.1,
+    #     k=0,
+    #     stop_sequences=[],
+    #     return_likelihoods='NONE'
+    # )
+    data = {"text":resp1,"text2":resp2}
     return jsonify(data)
 
 
@@ -55,6 +59,65 @@ def fullpage():
     
 #     # Redirect to the new page
 #     return redirect('/result')
+@app.route("/TextToImg.html")
+def TextToImg():
+    return render_template("TextToImg.html")
 
+
+# @app.route("/processimage", methods=['POST'])
+# def processimage():
+#    img = response.json
+#    url = img["url"]
+#    return jsonify(url)
+
+
+
+
+
+
+
+def easyexplain(var):
+  
+  # Set up your OpenAI API credentials
+  openai.api_key = os.getenv("OPENAI_API_KEY")
+
+  # Prompt for the model to generate child-like speech
+
+  #variable 
+  prompt = f"explain this concept like i am 2 year child , {var}"
+
+  # Generate text with GPT-3.5 model
+  response = openai.Completion.create(
+  engine="text-davinci-003",  # Specify the GPT model (e.g., text-davinci-003)
+  prompt=prompt,
+  max_tokens=100,  # Adjust the max tokens based on the desired response length
+  temperature=0.8,  # Controls the randomness of the generated output
+  n=1,  # Number of responses to generate
+  stop=None,  # Stop sequence to end the generated text (optional)
+  )
+  prompt_ana = f"create an analogy or example for this phrase : \n {var}"
+  resp2=generate_analogy(prompt_ana)
+  # Print the generated child-like response
+  return [response.choices[0].text.strip(), resp2]
+
+def generate_analogy(prompt):
+  openai.api_key = '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'
+
+    # Generate text with GPT-3.5 model
+  print(" analogy  ")
+  response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=200,
+        temperature=0.8,
+        n=1,
+        stop=None,
+    )
+
+    # Return the generated analogy
+  return response.choices[0].text.strip()
+
+ 
+    
 if __name__ == "__main__":
     app.run(debug=True)
